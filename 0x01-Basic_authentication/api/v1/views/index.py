ac54439fab90d28da95600b9665f6ea308dd8509
elyse502 <elyseniyibizi502@gmail.com>
2024-02-12 12:26:30 +0000

    Add solutions to Tasks: 1. Error handler: Unauthorized AND 2. Error handler: Forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,19 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    raise 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    raise 403 error
+    """
+    abort(403)
