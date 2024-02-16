5966ee1aa6a35d8e282cdb6efc29c6a6fee3cb47
elyse502 <elyseniyibizi502@gmail.com>
2024-02-16 09:08:41 +0000

    Add solution to Task 10. Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -5,6 +5,7 @@ from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
 from datetime import datetime, timedelta
 
+
 class SessionDBAuth(SessionExpAuth):
     """
     Definition of SessionDBAuth class that persists session data
