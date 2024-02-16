29af3eb03f9263ba138656de82187a249ddb84bc
NIYIBIZI Elysée <elyseniyibizi502@gmail.com>
2024-02-16 11:05:20 +0200

    Add solution to Task 10. Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ Python Module """
-
 from .session_exp_auth import SessionExpAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from models.user_session import UserSession
-
+from datetime import datetime, timedelta
 
 class SessionDBAuth(SessionExpAuth):
     """
@@ -17,15 +17,16 @@ class SessionDBAuth(SessionExpAuth):
         Args:
            user_id (str): user id
         """
-        session_id = super().create_session(user_id)
-        if not session_id:
+        # Doesn’t return a Session ID and don’t create any UserSession
+        # Record in DB if user_id = None
+        if user_id is None or not isinstance(user_id, str):
             return None
-        kw = {
-            "user_id": user_id,
-            "session_id": session_id
-        }
-        user = UserSession(**kw)
-        user.save()
+
+        session_id = super().create_session(user_id)
+        new_user_session = UserSession()
+        new_user_session.user_id = user_id
+        new_user_session.session_id = session_id
+        new_user_session.save()
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
@@ -35,10 +36,23 @@ class SessionDBAuth(SessionExpAuth):
         Return:
             user id or None if session_id is None or not a string
         """
-        user_id = UserSession.search({"session_id": session_id})
-        if user_id:
-            return user_id
-        return None
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        user_session = UserSession.search({'session_id': session_id})
+        # If the Session ID of the request is not linked to any User ID
+        if not user_session:
+            return None
+
+        user_json = user_session[0].to_json()
+
+        if self.session_duration <= 0:
+            return user_json.get('user_id')
+        created_at = datetime.fromisoformat(user_json.get('created_at'))
+        expiration_time = created_at + timedelta(seconds=self.session_duration)
+        if expiration_time < datetime.now():
+            return None
+        return user_json.get('user_id')
 
     def destroy_session(self, request=None):
         """ Destroy a UserSession instance based on a
@@ -47,10 +61,12 @@ class SessionDBAuth(SessionExpAuth):
         if request is None:
             return False
         session_id = self.session_cookie(request)
+        # Check if request doesn’t contain the Session ID cookie
         if not session_id:
             return False
-        user_session = UserSession.search({"session_id": session_id})
-        if user_session:
-            user_session[0].remove()
-            return True
-        return False
+        user_session = UserSession.search({'session_id': session_id})
+        # If the Session ID of the request is not linked to any User ID
+        if not user_session:
+            return False
+        user_session[0].remove()
+        return True
